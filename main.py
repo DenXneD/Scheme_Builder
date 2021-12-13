2f4fe48a4126177ba5368d86950a2821bc804b9a
Gratky <qqeewwrr1515@gmail.com>
2021-12-13 21:24:14 +0200

    fix branch

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Shape():
         self.color = color
         self.position = position
         self.length = length
+        self.connections = {}
 
     def paint(self, painter):
         pass
@@ -21,13 +22,13 @@ class Ellipse(Shape):
         painter.setBrush(QBrush(self.color))
         x, y = self.position.x(), self.position.y()
         painter.drawEllipse(x, y, self.length*2, self.length)
+        self.connections.update({"Up": [], "Down": []})
 
     def get_area(self):
         x_end = self.position.x() + self.length*2
         y_end = self.position.y() + self.length
         area = [self.position.x(), self.position.y(), x_end, y_end]
         return area
-
 
 
 class Window(QWidget):
@@ -48,7 +49,8 @@ class Window(QWidget):
             pos = QtCore.QPoint(event.pos().x()-45, event.pos().y()-25)
             length = 50
             color = QtGui.QColor(100, 100, 100)
-            self.shapes.append(Ellipse(length, pos, color))
+            new_shape = Ellipse(length, pos, color)
+            self.shapes.append(new_shape)
 
         elif event.button() == Qt.RightButton:
             x = event.pos().x()
@@ -66,3 +68,6 @@ if __name__ == "__main__":
     window = Window()
     window.show()
     sys.exit(App.exec())
+
+
+
