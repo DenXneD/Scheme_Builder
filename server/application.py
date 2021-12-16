9e5c0470a2a35762dfd7c7db71ce7a285d9286b9
ZinganDovgaluk <SoftMark228@gmail.com>
2021-12-16 20:42:01 +0200

    server: json parse fixes, ability to specify file path added

--- server/application.py
+++ server/application.py
@@ -5,43 +5,47 @@ from server.parser import SpringsToCodeParser
 
 class SpringsModel(object):
     @classmethod
-    def save_springs_to_pickle_file(cls, springs_json_list: list):
+    def save_springs_to_pickle_file(cls, file_path: str, springs_json_list: list):
         """
+        :param file_path: full path to file
+        :type file_path: str
+
         :param springs_json_list: list of springs to save
         :type springs_json_list: list[dict]
+
+        :return: None
         """
         SpringDAO.insert_springs(
+            file_path=file_path,
             springs=[Spring.parse(spring_json) for spring_json in springs_json_list]
         )
 
     @classmethod
-    def load_springs_from_pickle_file(cls):
+    def load_springs_from_pickle_file(cls, file_path: str):
         """
+        :param file_path: full path to file
+        :type file_path: str
+
         :return: list of springs json version
         :rtype: list[dict]
         """
-        return [spring.json for spring in SpringDAO.get_springs()]
+        return [spring.json() for spring in SpringDAO.get_springs(
+            file_path=file_path
+        )]
 
     @classmethod
-    def generate_springs_python_file(cls, springs_json_list: list):
+    def generate_python_file_and_return_code(cls, file_path: str, springs_json_list: list):
         """
+        :param file_path: full path to file
+        :type file_path: str
+
         :param springs_json_list: list of springs to convert
         :type springs_json_list: list[dict]
 
         :return: generated file text
         :rtype: str
         """
-        return SpringsToCodeParser.parse_springs_to_code(
+        return SpringsToCodeParser.generate_python_file_from_springs_and_return_code(
+            file_path=file_path,
             springs=[Spring.parse(spring_json) for spring_json in springs_json_list]
         )
-
-    @classmethod
-    def generate_code_for_spring(cls, spring_json):
-        """
-        :param spring_json: spring to convert
-        :type spring_json: dict
-
-        :return: generated file text
-        :rtype: str
-        """
-        return SpringsToCodeParser.parse_springs_to_code(springs=[Spring.parse(spring_json)])
